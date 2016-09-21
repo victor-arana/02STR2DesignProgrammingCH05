a91f31f0ae20027a64f944ea80263c6c93ece644
Victor Jose Arana Rodriguez <victor_arana@hotmail.com>
2016-09-21 12:27:51 -0500

    ttt

--- src/main/java/com/struts2/designprogramming/RadioTestAction.java
+++ src/main/java/com/struts2/designprogramming/RadioTestAction.java
@@ -15,7 +15,7 @@ public class RadioTestAction extends ActionSupport {
         incomeLevels = new TreeMap<Integer, String>();
         incomeLevels.put(1, "0 - $10,000");
         incomeLevels.put(2, "$10,001 - $30,000");
-        incomeLevels.put(3, "$30,001 - $50,000");
+        incomeLevels.put(3, "$30,001 - $50,0000");
         incomeLevels.put(4, "Over $50,000");
     }
     public int getIncomeLevel() {
